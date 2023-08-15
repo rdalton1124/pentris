638bf84220bee66b19416c49f50abbf8d2fbf7dc
Dalton, Rebecca Lynne <rebedalt@iu.edu>
2023-08-15 18:46:46 -0400

    Added music and toggle
    - Added standard tetris music
    - Music plays when game starts and loops when donhe
    - Music will not play if music turned off

--- scenes/AudioController.gd
+++ scenes/AudioController.gd
@@ -3,13 +3,14 @@ extends Node2D
 @onready var qdSound = get_node("../audio/quickDrop")
 @onready var bumpSound = get_node("../audio/wall_bump")
 @onready var lineSound = get_node("../audio/line") 
-
+@onready var music = get_node("../audio/musicAudio")
 var soundOn
 var musicOn 
 
 func _ready(): 
 	soundOn = true
 	musicOn = true
+	music.play()
 
 func toggleSound():
 	soundOn = not soundOn
@@ -26,3 +27,9 @@ func line():
 func bump(): 
 	if soundOn:
 		bumpSound.play() 	
+func _process(delta):
+	if not musicOn: 
+		music.stop() 
+	elif musicOn and not music.is_playing():
+		music.play() 
+	
--- scenes/music.gd
+++ scenes/music.gd
@@ -2,13 +2,11 @@ extends Button
 
 @onready var on = self.get_node("musicOn")
 @onready var off = self.get_node("musicOff")
-@onready var isMusic = true 
-
+@onready var ac = get_node("../AudioController")
 func _ready():
 	off.visible = false
 	
 func _toggled(button_pressed):
-	isMusic = not isMusic
-	on.visible = isMusic
-	off.visible = not isMusic
-	
+	ac.toggleMusic()
+	on.visible = not on.visible
+	off.visible = not off.visible 	
